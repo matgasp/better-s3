8352a0fcd4451751362df285670f06a4ad9e8c67
Matt <matgasp@hotmail.com>
2024-05-16 22:16:38 -0300

    Updated publishing files;

--- setup.py
+++ setup.py
@@ -5,23 +5,22 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\\n" + fh.read()
+  long_description = "\\n" + fh.read()
 
 setup(
-    name="betters3",
-    version='{{VERSION_PLACEHOLDER}}',
-    author="matgasp",
-    description="A wrapper of AWS S3 (boto3) that facilitate some operations. ",
-    url = "https://github.com/matgasp/betters3",
-    long_description_content_type="text/markdown",
-    long_description=long_description,
-    packages=find_packages(),
-    install_requires=['numpy'],
-    keywords=['pypi', 'cicd', 'python'],
-    classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ]
+  name="betters3",
+  version='{{VERSION_PLACEHOLDER}}',
+  author="matgasp",
+  description="A wrapper of AWS S3 (boto3) that facilitate some operations. ",
+  url = "https://github.com/matgasp/betters3",
+  long_description_content_type="text/markdown",
+  long_description=long_description,
+  packages=find_packages(),
+  install_requires=['boto3', 'botocore'],
+  classifiers=[
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+  ]
 )
