b513427335eefe1ff74b56944f35d3b3c3662d7f
Matt <matgasp@hotmail.com>
2024-05-17 09:46:31 -0300

    Organizing the files; Added LICENSE; Fixed setup.py;

--- setup.py
+++ setup.py
@@ -12,13 +12,21 @@ setup(
   author="matgasp",
   version='{{VERSION_PLACEHOLDER}}',
   url = "https://github.com/matgasp/betters3",
-  description="A wrapper of AWS S3 (boto3) that facilitate some operations. ",
+  description="A wrapper of AWS S3 (boto3) that facilitate some operations.",
   install_requires=['boto3', 'botocore'],
+  keywords=['better', 's3', 'better s3', 'betters3'],
   
   long_description_content_type="text/markdown",
   long_description=long_description,
   packages=find_packages(),
   classifiers=[
+    "Operating System :: Microsoft :: Windows",
+    "Intended Audience :: Developers",
+    "Development Status :: 5 - Production/Stable",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Topic :: Utilities",
+    
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
